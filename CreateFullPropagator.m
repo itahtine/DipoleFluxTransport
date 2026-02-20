1943e2ed367c8153fbf5473787f414aba2b81099
itahtine <33904534+itahtine@users.noreply.github.com>
2026-02-20 16:52:55 +0200

    Add files via upload

---
+++ CreateFullPropagator.m
@@ -0,0 +1,23 @@
+%%
+load('VectorMapSliceDiff350Mer11CR')
+
+VectorMap = cat(1,repmat(VectorMapSlice,1,360,1,1),flip(repmat(VectorMapSlice,1,360,1,1),1));
+VectorMap(91:180,:,2,:) = -VectorMap(91:180,:,2,:);
+
+for i = 1:360
+    VectorMap(:,i,3,:) = VectorMap(:,i,3,:)+(i-1);
+end
+
+%save('VectorMapFullDiff350Mer11CR','VectorMap')
+%%
+load('VectorMapSliceDiff350Mer11Daily')
+
+VectorMap = cat(1,repmat(VectorMapSlice,1,360,1,1),flip(repmat(VectorMapSlice,1,360,1,1),1));
+VectorMap(91:180,:,2,:) = -VectorMap(91:180,:,2,:);
+
+for i = 1:360
+    VectorMap(:,i,3,:) = VectorMap(:,i,3,:)+(i-1);
+end
+
+%save(VectorMapFullDiff350Mer11Daily','VectorMap')
+%%
